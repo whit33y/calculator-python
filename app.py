092573dbee1f48049a63b5dc572db6f85e83ee20
whit33y <troppout@gmail.com>
2022-04-28 22:16:23 +0200

    More math operations

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, redirect
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, SubmitField , validators
+from wtforms import IntegerField, SubmitField , SelectField, validators
 app=Flask(__name__)
 
 app.secret_key='xd'
@@ -11,9 +11,33 @@ def calc():
     if form.validate_on_submit():
         one = form.number_one.data
         two = form.number_two.data
-        result = one + two
+        math_operations = form.math_operations.data
+        if math_operations == "+":
+            operation_name = 'Addition: '
+            result = one + two
+            percent = ''
+        elif math_operations == "-":
+            operation_name = 'Subtraction: '
+            result = one - two
+            percent = ''
+        elif math_operations == "*":
+            operation_name = 'Multiplication: '
+            result = one * two
+            percent = ''
+        elif math_operations == "/":
+            operation_name = 'Division : '
+            result = one / two
+            percent = ''
+        elif math_operations == "%":
+            operation_name = 'Percent: '
+            result = 100 * float(one)/float(two)
+            percent = '%'
+        elif math_operations == "**":
+            operation_name = 'Power: '
+            result = one ** two
+            percent = ''
         # return redirect(url_for('res', result=result))
-        return render_template('result.html',result=result)
+        return render_template('result.html',operation_name=operation_name,result=result,percent=percent)
     return render_template('index.html', form=form)
 
 @app.errorhandler(404)
@@ -25,6 +49,15 @@ def not_found(error):
 #     return render_template('result.html')
 
 class your_numbers(FlaskForm):
+    math_operations = [
+            ('+', '+'),
+            ('-', '-'),
+            ('*', '*'),
+            ('/', '/'),
+            ('%', '%'),
+            ('**', '**')
+        ]
+    math_operations = SelectField('Chose operation: ', [validators.DataRequired()], choices=math_operations,)
     number_one = IntegerField('Your first number: ', [validators.DataRequired()])
     number_two = IntegerField('Your second number: ',[validators.DataRequired()])
     button = SubmitField('Result')
